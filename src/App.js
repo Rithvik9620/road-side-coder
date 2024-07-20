5bb799d54098e66e2ed3727e5555e96f923a3049
Rithvik <rithvikgoud7@gmail.com>
2024-07-20 20:26:38 +0530

    File System completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import explorerData from "./data/explorerData";
+import Folder from "./components/Folder";
+import useTreeHook from "./hooks/use-tree-hook";
 
 function App() {
+  const [explorerDateTree, setExplorerDataTree] = useState(explorerData);
+  const { addIntoTree } = useTreeHook();
+
+  const handleAddNew = (folderId, name, isFolder) => {
+    let updatedTree = addIntoTree(explorerDateTree, folderId, name, isFolder);
+    setExplorerDataTree(updatedTree);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Folder explorerData={explorerDateTree} handleAddNew={handleAddNew} />
     </div>
   );
 }
---
+++ src/data/explorerData.js
@@ -0,0 +1,15 @@
+const explorerData = {
+  id: 1,
+  name: "root",
+  isFolder: true,
+  items: [
+    {
+      id: 2,
+      name: "public",
+      isFolder: true,
+      items: [],
+    },
+  ],
+};
+
+export default explorerData;
